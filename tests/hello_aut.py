3ab811fdb12a1361604d4177dd01c077246bb52a
rivaperma <permataxrifa@gmail.com>
2026-07-13 10:20:23 +0700

    tambah pengujian paralel browser

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -4,14 +4,20 @@ from selenium.webdriver.common.by import By
 
 class AutTest(unittest.TestCase):
 
-    def setUp(self):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
-        server = 'http://localhost:4444'
-
-        self.browser = webdriver.Remote(command_executor=server, options=options)
-        self.addCleanup(self.browser.quit)
+def setUp(self):
+    browser_name = sys.argv[2] if len(sys.argv) > 2 else "firefox"
+    options = webdriver.FirefoxOptions() # Default Firefox
+    
+    if browser_name == "chrome":
+        options = webdriver.ChromeOptions()
+    elif browser_name == "edge":
+        options = webdriver.EdgeOptions()
+        
+    options.add_argument('--ignore-ssl-errors=yes')
+    options.add_argument('--ignore-certificate-errors')
+    server = 'http://localhost:4444'
+    self.browser = webdriver.Remote(command_executor=server, options=options)
+    self.addCleanup(self.browser.quit)
 
     def test_homepage(self):
         if len(sys.argv) > 1:
